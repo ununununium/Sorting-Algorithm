bb3f1d73750a2a2fa694eb3099e0b3a1e71f664a
Boyan Liu <lbyybl1996@gmail.com>
2020-07-19 19:14:32 -0400

    add css styling in home screen

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,33 +1,40 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-
-const Cell = ({ nav, title, callBack }) => {
-	return (
-		<TouchableOpacity style={styles.cell} onPress={() => callBack()}>
-			<Text style={styles.cellText}>{title}</Text>
-			<Ionicons name="ios-arrow-forward" style={styles.cellButton} />
-		</TouchableOpacity>
-	);
+const Cell = ({ title, callBack, color }) => {
+  return (
+    <View>
+      <TouchableOpacity
+        style={[styles.cell, { backgroundColor: color }]}
+        onPress={() => callBack()}>
+        <Text style={styles.cellText}>{title}</Text>
+        <Ionicons name="ios-arrow-forward" style={styles.cellButton} />
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-	cell: {
-		flexDirection: "row",
-		justifyContent: "space-between",
-		paddingHorizontal: 20,
-		paddingTop: 10,
-		backgroundColor: "white",
-		borderBottomWidth: 1,
-		borderBottomColor: "rgb(233,233,233)",
-	},
-	cellText: {
-		fontSize: 18,
-	},
-	cellButton: {
-		fontSize: 24,
-		color: "black",
-	},
+  cell: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginHorizontal: 15,
+    paddingHorizontal: 15,
+    paddingTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "rgb(233,233,233)",
+    height: 160,
+    width: 160,
+    borderRadius: 20,
+  },
+  cellText: {
+    fontSize: 18,
+    color: "#fff",
+  },
+  cellButton: {
+    fontSize: 24,
+    color: "black",
+  },
 });
 
 export default Cell;
