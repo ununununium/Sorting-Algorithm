51380e7115e727c7bd6625a37ebab2663941b262
Yuting Zhong <izhongyuting@gmail.com>
2020-07-20 01:13:51 -0400

    color added

--- src/components/DynamicBar.js
+++ src/components/DynamicBar.js
@@ -15,24 +15,35 @@ const DEFAULT_BARS = [76, 44, 96, 55, 84, 66, 41, 52, 88, 77];
 
 //........................Sort........................
 
-const sleep = (milliseconds) => {
-	return new Promise((resolve) => setTimeout(resolve, milliseconds));
-};
-async function bubbleSort(swap, A) {
-	var n = A.length;
+async function bubbleSort(swap, A, visit, unvisit) {
+	const sleep = (milliseconds) => {
+		return new Promise((resolve) => setTimeout(resolve, milliseconds));
+	};
+
+	let n = A.length;
 
 	for (var i = 0; i < n - 1; i++) {
 		for (var j = 0; j < n - i - 1; j++) {
-			if (parseInt(A[j]._value) > parseInt(A[j + 1]._value)) {
+			let v1 = A[j].val;
+			let v2 = A[j + 1].val;
+			if (parseInt(v1._value) > parseInt(v2._value)) {
+				visit(j);
+				visit(j + 1);
+				await sleep(300);
 				swap(j, j + 1);
+				await sleep(300);
+				unvisit(j);
+				unvisit(j + 1);
 				await sleep(300);
 			}
 		}
 	}
 }
-//........................Sort........................
+
+//.....................................................
 
 const DynamicBar = () => {
+	//........................ Bar Width Animation ........................
 	const animWidth = useState(
 		new Animated.Value((WIDTH - 40) / DEFAULT_BARS.length)
 	)[0];
@@ -45,30 +56,60 @@ const DynamicBar = () => {
 		}).start();
 	}
 
+	//........................ Bar Value init ........................
 	const animVals = [];
+	for (var i = 0; i < DEFAULT_BARS.length; i++) animVals[i] = {};
+
 	for (var i = 0; i < DEFAULT_BARS.length; i++) {
-		animVals[i] = useState(new Animated.Value(DEFAULT_BARS[i]))[0];
+		animVals[i].val = useState(new Animated.Value(DEFAULT_BARS[i]))[0];
 	}
 
+	for (var i = 0; i < DEFAULT_BARS.length; i++) {
+		animVals[i].colorRaw = useState(new Animated.Value(0))[0];
+		animVals[i].colorInterpolation = animVals[i].colorRaw.interpolate({
+			inputRange: [0, 1],
+			outputRange: ["rgb(0,0,0)", "rgb(0,255,0)"],
+		});
+	}
+
+	//........................ animVals Getter and Setter ........................
 	const [getAnimVal, SetAnimVal] = useState(animVals);
-
+	//........................ Bar Swap Animation ........................
 	function animSwap(a, b) {
-		const a_val = getAnimVal[a]._value;
-		const b_val = getAnimVal[b]._value;
+		const a_val = getAnimVal[a].val._value;
+		const b_val = getAnimVal[b].val._value;
 
-		Animated.timing(getAnimVal[a], {
+		Animated.timing(getAnimVal[a].val, {
 			toValue: b_val,
 			duration: 300,
 			useNativeDriver: false,
 		}).start();
 
-		Animated.timing(getAnimVal[b], {
+		Animated.timing(getAnimVal[b].val, {
 			toValue: a_val,
 			duration: 300,
 			useNativeDriver: false,
 		}).start();
 	}
+	//........................ Bar Visit Animation ........................
 
+	function visit(idx) {
+		Animated.timing(getAnimVal[idx].colorRaw, {
+			toValue: 1,
+			duration: 200,
+			useNativeDriver: false,
+		}).start();
+	}
+
+	function unvisit(idx) {
+		Animated.timing(getAnimVal[idx].colorRaw, {
+			toValue: 0,
+			duration: 200,
+			useNativeDriver: false,
+		}).start();
+	}
+
+	//........................ Dynamic Bar Render ........................
 	return (
 		<View style={styles.frame}>
 			<View style={styles.barCollection}>
@@ -83,8 +124,9 @@ const DynamicBar = () => {
 							<Animated.View
 								style={{
 									...styles.bar,
-									height: item,
+									height: item.val,
 									width: animWidth,
+									backgroundColor: item.colorInterpolation,
 								}}
 							></Animated.View>
 						);
@@ -109,7 +151,7 @@ const DynamicBar = () => {
 					onPress={() => {
 						let barNum = getAnimVal.length;
 						let newVal = Math.floor(Math.random() * 70 + 30);
-						SetAnimVal([...getAnimVal, new Animated.Value(newVal)]);
+						SetAnimVal([...getAnimVal, { val: new Animated.Value(newVal) }]);
 						changeBarWidth(barNum + 1);
 					}}
 				>
@@ -120,7 +162,7 @@ const DynamicBar = () => {
 			<Button
 				title="Sort It !!!"
 				onPress={() => {
-					bubbleSort(animSwap, getAnimVal);
+					bubbleSort(animSwap, getAnimVal, visit, unvisit);
 				}}
 			/>
 		</View>
