9788304588b25fa4f3560ec968da0b84a10f1055
Yuting Zhong <izhongyuting@gmail.com>
2020-07-23 23:39:52 -0400

    animation tab

--- src/components/SortTitle.js
+++ src/components/SortTitle.js
@@ -1,132 +1,135 @@
 import React, { useState } from "react";
 import {
-  Text,
-  StyleSheet,
-  View,
-  TouchableOpacity,
-  Animated,
+	Text,
+	StyleSheet,
+	View,
+	TouchableOpacity,
+	Animated,
 } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 const SortTitle = ({ callBack, title }) => {
-  const [flag, setFlag] = useState([false, true, false]);
-  const opacity = useState(new Animated.Value(0))[0];
-  const handleSpeedBtn = (index) => {
-    let ary = [false, false, false];
-    ary[index] = ary[index] == true ? false : true;
-    setFlag(ary);
-    Animated.timing(opacity, {
-      toValue: 1,
-      duration: 2000,
-      useNativeDriver: true,
-    }).start();
-  };
-  return (
-    <View>
-      <View style={styles.titleContainer}>
-        <Text style={styles.titleText}>{title}</Text>
-      </View>
+	const [flag, setFlag] = useState([false, true, false]);
+	const opacity = useState(new Animated.Value(0))[0];
+	const handleSpeedBtn = (index) => {
+		let ary = [false, false, false];
+		ary[index] = ary[index] == true ? false : true;
+		setFlag(ary);
+		Animated.timing(opacity, {
+			toValue: 1,
+			duration: 2000,
+			useNativeDriver: true,
+		}).start();
+	};
+	return (
+		<View>
+			<View style={styles.titleContainer}>
+				<Text style={styles.titleText}>{title}</Text>
+			</View>
 
-      <View style={styles.btnGroupContainer}>
-        <View style={styles.btnContainer}>
-          <TouchableOpacity onPress={() => handleSpeedBtn(0)}>
-            <Animated.View
-              style={[flag[0] ? styles.buttonTextLayer : {}, opacity]}>
-              <MaterialCommunityIcons
-                name="speedometer-slow"
-                size={46}
-                color="white"
-                style={[flag[0] ? styles.icon : {}]}
-              />
-              <Text style={[styles.buttonText, flag[0] ? {} : styles.hidden]}>
-                Slow
-              </Text>
-            </Animated.View>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.btnContainer}>
-          <TouchableOpacity onPress={() => handleSpeedBtn(1)}>
-            <Animated.View
-              style={[flag[1] ? styles.buttonTextLayer : {}, opacity]}>
-              <MaterialCommunityIcons
-                name="speedometer-medium"
-                size={46}
-                color="white"
-                style={[flag[1] ? styles.icon : {}]}
-              />
-              <Text style={[styles.buttonText, flag[1] ? {} : styles.hidden]}>
-                Medium
-              </Text>
-            </Animated.View>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.btnContainer}>
-          <TouchableOpacity onPress={() => handleSpeedBtn(2)}>
-            <Animated.View
-              style={[flag[2] ? styles.buttonTextLayer : {}, opacity]}>
-              <MaterialCommunityIcons
-                name="speedometer"
-                size={46}
-                color="white"
-                style={[flag[2] ? styles.icon : {}]}
-              />
+			<View style={styles.btnGroupContainer}>
+				<View style={styles.btnContainer}>
+					<TouchableOpacity onPress={() => handleSpeedBtn(0)}>
+						<Animated.View
+							style={[flag[0] ? styles.buttonTextLayer : {}, opacity]}
+						>
+							<MaterialCommunityIcons
+								name="speedometer-slow"
+								size={46}
+								color="white"
+								style={[flag[0] ? styles.icon : {}]}
+							/>
+							<Text style={[styles.buttonText, flag[0] ? {} : styles.hidden]}>
+								Slow
+							</Text>
+						</Animated.View>
+					</TouchableOpacity>
+				</View>
+				<View style={styles.btnContainer}>
+					<TouchableOpacity onPress={() => handleSpeedBtn(1)}>
+						<Animated.View
+							style={[flag[1] ? styles.buttonTextLayer : {}, opacity]}
+						>
+							<MaterialCommunityIcons
+								name="speedometer-medium"
+								size={46}
+								color="white"
+								style={[flag[1] ? styles.icon : {}]}
+							/>
+							<Text style={[styles.buttonText, flag[1] ? {} : styles.hidden]}>
+								Medium
+							</Text>
+						</Animated.View>
+					</TouchableOpacity>
+				</View>
+				<View style={styles.btnContainer}>
+					<TouchableOpacity onPress={() => handleSpeedBtn(2)}>
+						<Animated.View
+							style={[flag[2] ? styles.buttonTextLayer : {}, opacity]}
+						>
+							<MaterialCommunityIcons
+								name="speedometer"
+								size={46}
+								color="white"
+								style={[flag[2] ? styles.icon : {}]}
+							/>
 
-              <Text style={[styles.buttonText, flag[2] ? {} : styles.hidden]}>
-                Fast
-              </Text>
-            </Animated.View>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </View>
-  );
+							<Text style={[styles.buttonText, flag[2] ? {} : styles.hidden]}>
+								Fast
+							</Text>
+						</Animated.View>
+					</TouchableOpacity>
+				</View>
+			</View>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  titleText: {
-    fontSize: 25,
-    color: "#000000",
-    fontWeight: "bold",
-    marginLeft: 10,
-    marginVertical: 5,
-  },
-  titleContainer: {
-    borderBottomColor: "#e3e3e3",
-    borderBottomWidth: 2,
-  },
-  icon: {
-    marginLeft: 10,
-  },
-  textDiv: {},
+	titleText: {
+		fontSize: 25,
+		color: "#000000",
+		fontWeight: "bold",
+		marginLeft: 10,
+		marginVertical: 5,
+	},
+	titleContainer: {
+		borderBottomColor: "#e3e3e3",
+		borderBottomWidth: 2,
+	},
+	icon: {
+		marginLeft: 10,
+	},
+	textDiv: {},
 
-  btnGroupContainer: {
-    height: 80,
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-around",
-    backgroundColor: "#889bf2",
-  },
-  btnContainer: {
-    paddingVertical: 10,
-    justifyContent: "space-around",
-  },
-  buttonText: {
-    textAlign: "center",
-    color: "#ffffff",
-    fontSize: 15,
-    alignSelf: "center",
-    flex: 1,
-  },
-  buttonTextLayer: {
-    width: 130,
-    backgroundColor: "#5c69a4",
-    borderRadius: 50,
-    flexDirection: "row",
-  },
-  hidden: {
-    width: 0,
-    height: 0,
-  },
+	btnGroupContainer: {
+		height: 80,
+		flex: 1,
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "space-around",
+		backgroundColor: "#889bf2",
+	},
+	btnContainer: {
+		paddingVertical: 10,
+		justifyContent: "space-around",
+	},
+	buttonText: {
+		textAlign: "center",
+		color: "#ffffff",
+		fontSize: 15,
+		alignSelf: "center",
+		flex: 1,
+	},
+	buttonTextLayer: {
+		width: 130,
+		backgroundColor: "#5c69a4",
+		borderRadius: 50,
+		flexDirection: "row",
+	},
+	hidden: {
+		width: 0,
+		height: 0,
+	},
 });
 
 export default SortTitle;
--- src/screens/BubbleSortScreen.js
+++ src/screens/BubbleSortScreen.js
@@ -1,15 +1,80 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+import {
+	StyleSheet,
+	View,
+	TouchableOpacity,
+	Animated,
+	Dimensions,
+} from "react-native";
 import DynamicBar from "../components/DynamicBar";
 
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+var WIDTH = Dimensions.get("window").width;
+
 const BubbleSortScreen = () => {
+	const tabPos = useState(new Animated.Value(20))[0];
+
+	function changeTabPos(v) {
+		Animated.spring(tabPos, {
+			toValue: v,
+			duration: 200,
+			useNativeDriver: false,
+		}).start();
+	}
+
 	return (
 		<View>
-			<DynamicBar sortAlgo="Bubble Sort" />
+			<View style={styles.numBar}>
+				<DynamicBar sortAlgo="Bubble Sort" hideButton />
+			</View>
+			<View style={styles.speedTabBar}>
+				<Animated.View
+					style={{ ...styles.speedTabHighLighter, left: tabPos }}
+				/>
+				<TouchableOpacity onPress={() => changeTabPos(20)}>
+					<MaterialCommunityIcons
+						name="speedometer-slow"
+						size={46}
+						color="white"
+					/>
+				</TouchableOpacity>
+				<TouchableOpacity onPress={() => changeTabPos(WIDTH / 3)}>
+					<MaterialCommunityIcons
+						name="speedometer-medium"
+						size={46}
+						color="white"
+					/>
+				</TouchableOpacity>
+				<TouchableOpacity onPress={() => changeTabPos((2 * WIDTH) / 3 - 20)}>
+					<MaterialCommunityIcons name="speedometer" size={46} color="white" />
+				</TouchableOpacity>
+			</View>
 		</View>
 	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	numBar: {
+		paddingTop: 30,
+		paddingBottom: 30,
+	},
+	speedTabBar: {
+		height: 70,
+		backgroundColor: "#889BF2",
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "space-around",
+	},
+	speedTabHighLighter: {
+		height: 50,
+		width: WIDTH / 3,
+		backgroundColor: "#000000",
+		opacity: 0.32,
+		borderRadius: 20,
+		left: (2 * WIDTH) / 3 - 20,
+		position: "absolute",
+	},
+});
 
 export default BubbleSortScreen;
